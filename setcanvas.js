aa6e17c3596635aee7370b8499107f7b5834ab48
caijianhong <86008072+caijianhong@users.noreply.github.com>
2021-07-06 23:12:57 +0800

    Delete setcanvas.js

--- setcanvas.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$(window).resize(resizecanvas);
-	resizecanvas()
-});
-var resizecanvas=function(){
-	$("#canvas").attr("width",$(window).get(0).innerWidth);
-    $("#canvas").attr("height",$(window).get(0).innerHeight);
-}
