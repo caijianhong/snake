fb29978b30f1f830f9e3c07e54afd65af94492d9
caijianhong <86008072+caijianhong@users.noreply.github.com>
2021-07-07 23:40:06 +0800

    Update listener.js

--- js/listener.js
+++ js/listener.js
@@ -1,24 +1,37 @@
-const keys={
-	65:"A",	74:"J",	83:"S",	49:"1",
-	66:"B",	75:"K",	84:"T",	50:"2",
-	67:"C",	76:"L",	85:"U",	51:"3",
-	68:"D",	77:"M",	86:"V",	52:"4",
-	69:"E",	78:"N",	87:"W",	53:"5",
-	70:"F",	79:"O",	88:"X",	54:"6",
-	71:"G",	80:"P",	89:"Y",	55:"7",
-	72:"H",	81:"Q",	90:"Z",	56:"8",
-	73:"I",	82:"R",	48:"0",	57:"9"
-};
-const map={
-	37:2,38:1,39:3,40:4,//左上右下
-	87:1,65:2,83:4,68:3 //WASD
-};
+var isstop=false;
 $("body").keydown(function(e){
 	console.log("user clicks key "+e.keyCode);
-	var tmp=map[e.keyCode];
-	if(tmp!=undefined&&moved==false) snake.setdir(tmp);
 	var key=keys[e.keyCode];
-	if(key=="C") nowtext=[];
-	if(key=="P") score++;
-	if(key=="M") apple.move();
+	if(stage==1){
+		if(key=="Enter"){
+			stage++;
+			clearInterval(interval);
+			interval=setInterval(startgame,speed);
+		}
+	}else if(stage==2){
+		var tmp=map[e.keyCode];
+		if(tmp!=undefined&&moved==false) snake.setdir(tmp);
+		if(key=="C") nowtext=[];
+		if(key=="P") score++;
+		if(key=="M") apple.move();
+		if(key=="B"){
+			if(isstop==true){
+				isstop=false;
+				interval=setInterval(startgame,speed);
+			}else{
+				isstop=true;
+				clearInterval(interval);
+			}
+			console.log("Stop or start?"+isstop);
+		}
+	}else if(stage==3){
+		if(key=="Enter"){
+			stage++;
+			aftergame();
+		}
+	}else if(stage==4){
+		
+	}else{
+		console.log("Error: Stage isn't in range [1,4]!");
+	}
 });
