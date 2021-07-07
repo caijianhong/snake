586deb35543fb2bb1da3242b81aae1987009f1ec
caijianhong <86008072+caijianhong@users.noreply.github.com>
2021-07-07 23:37:20 +0800

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,9 +1,17 @@
 var gameover=function(){
 	clearInterval(interval);
-	alert("你失败了！");
+	ctx.font="30px consolas";
+	ctx.textBaseline="top";
+	ctx.textAlign="left";
+	ctx.fillStyle="black";
+	ctx.fillText("游戏失败，3 秒后重新开始......",10,height-50);
+	setTimeout(function(){
+		window.location.reload();
+	},3000);
 }
 var win=function(){
 	console.log("win!");
+	stage++;
 	clearInterval(interval);
 	var img=new Image();
 	img.src="img/end.jpg";
@@ -12,15 +20,27 @@ var win=function(){
 		ctx.drawImage(img,0,0,width,height);
 	}
 }
-var drawstar=function(){
-	var img=new Image();
-	img.src="img/star.png";
-	img.onload=function(){
-		var y=0;
-		for(var i=0;i<score+1;i++){
-			ctx.drawImage(img,y,0);
-			y+=100;
-		}
+var drawstar=function(x,y){
+	var R=50,r=20;
+	ctx.beginPath();
+	for(var i=0;i<5;i++){
+	    ctx.lineTo(Math.cos((18+i*72)/180*Math.PI)*R+x,
+	               -Math.sin((18+i*72)/180*Math.PI)*R+y);
+	    ctx.lineTo(Math.cos((54+i*72)/180*Math.PI)*r+x,
+	               -Math.sin((54+i*72)/180*Math.PI)*r+y);
+	}   
+	ctx.closePath();
+	ctx.lineWidth="3";
+	ctx.fillStyle="#F6F152";
+	ctx.strokeStyle="#F5270B";
+	ctx.fill(); 
+	ctx.stroke();
+}
+var drawstars=function(){
+	var x=60;
+	for(var i=0;i<score+1;i++){
+		drawstar(x,60);
+		x+=100;
 	}
 }
 var Text=function(x,y,t,c){
@@ -39,17 +59,17 @@ Text.prototype.draw=function(){
 	if(this.x<0) return true;
 	return false;
 }
-var nowtext=[];
-var drawtext=function(){
+var nowbarrages=[];
+var drawbarrages=function(){
 	if(Math.random()<0.03){
-		nowtext.push(new Text(
+		nowbarrages.push(new Text(
 			width+100,
 			Math.floor(Math.random()*height),
 			texts[Math.floor(Math.random()*texts.length)],
 			colors[Math.floor(Math.random()*colors.length)]
 		));
 	}
-	for(var i=0;i<nowtext.length;i++){
-		if(nowtext[i].draw()) nowtext.splice(i,1);
+	for(var i=0;i<nowbarrages.length;i++){
+		if(nowbarrages[i].draw()) nowbarrages.splice(i,1);
 	}
 }
