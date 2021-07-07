baacc1eb7d949dbdf299f89a8f686acf7275e188
caijianhong <86008072+caijianhong@users.noreply.github.com>
2021-07-07 23:43:10 +0800

    Update log.js

--- js/log.js
+++ js/log.js
@@ -1,3 +1,4 @@
-console.log("made by caijianhong && huangyanglong");
-console.log("github: https://github.com/caijianhong/snake");
-console.log("license: MIT License");
+console.log("Made by caijianhong(code) and huangyanglong(text && image)");
+console.log("Github: https://github.com/caijianhong/snake");
+console.log("License: MIT License");
+console.log("Last update on 2021/7/7");
